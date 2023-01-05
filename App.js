33984878c1bc9a87926b028f3431f4d682ba1421
Никита <mnd1work@gmail.com>
2023-01-05 19:48:02 +0400

    commit 5.1.23 (1)

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import Front from './src/panels/Front'
+
 import global from './src/styles/Global-style'
 
 export default function App() {
---
+++ src/panels/Front.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+import global from '../styles/Global-style'
+// import global from './src/styles/Global-style'
+
+export default function Front() {
+    return (
+      <View style={global.container} >
+        
+      </View>
+    );
+}
